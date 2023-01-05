e44fa6950d54fcf3b6e069a24f6dcc097d366eb8
Abdel <mokbe004@umn.edu>
2023-01-05 17:35:31 -0600

    go solution

--- 2022/Day 1/Day1Code.go
+++ 2022/Day 1/Day1Code.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
+	"sort"
+	"strconv"
 	"strings"
 )
 
@@ -15,12 +18,36 @@ func reader() []string {
 
 func part1() {
 	f := reader()
-	fmt.Println(f)
+	ans, s := -1, 0
+	for _, l := range f {
+		if l == "" {
+			ans = int(math.Max(float64(ans), float64(s)))
+			s = 0
+			continue
+		}
+		i, _ := strconv.ParseInt(l, 10, 32)
+		s += int(i)
+	}
+	fmt.Println(ans)
 }
 
 func part2() {
 	f := reader()
-	fmt.Println(f)
+	var a []int
+	s := 0
+	for _, l := range f {
+		if l == "" {
+			a = append(a, s)
+			s = 0
+			continue
+		}
+		i, _ := strconv.ParseInt(l, 10, 32)
+		s += int(i)
+	}
+	sort.Slice(a, func(i, j int) bool {
+		return a[i] > a[j]
+	})
+	fmt.Println(a[0] + a[1] + a[2])
 }
 
 func main() {
