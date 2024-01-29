409ca4ee575dfc8eb592e9b66d8aa508b3485231
AbdeI1 <abdelrahmanmmokbel@gmail.com>
2024-01-29 00:32:02 -0600

    day 5

--- 2023/Day 5/code.py
+++ 2023/Day 5/code.py
@@ -21,8 +21,23 @@ class RangeMap:
       return 0
     if isinstance(key, list):
       res = []
-      for r, v in self.ranges:
-      return res
+      for r2 in key:
+        ints = []
+        for r1, v in self.ranges:
+          start = max(r1.start, r2.start)
+          stop = min(r1.stop, r2.stop)
+          if stop > start:
+            ints.append(range(start, stop))
+            res.append(range(start + v, stop + v))
+        ints.sort(key=lambda r: r.start)
+        start = r2.start
+        for i in ints:
+          if i.start > start:
+            res.append(range(start, i.start))
+          start = i.stop
+        if r2.stop > start:
+          res.append(range(start, r2.stop))
+      return sorted(res, key=lambda r: r.start)
 
 def part1():
   f = '\n'.join(reader()).split('\n\n')
@@ -54,12 +69,13 @@ def part2():
       r[range(nums[1], nums[1] + nums[2])] = nums[0] - nums[1]
     maps.append(r)
   ans = float('inf')
-  for s in seeds:
-    n = s
-    for m in maps:
-      n = n + m[n]
-    ans = min(ans, n)
-  print(ans)
+  ranges = []
+  for i in range(0, len(seeds), 2):
+    ranges.append(range(seeds[i], seeds[i] + seeds[i+1]))
+  ranges.sort(key = lambda r: r.start)
+  for m in maps:
+    ranges = m[ranges]
+  print(ranges[0].start)
 
 part1()
 part2()
