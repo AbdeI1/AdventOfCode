d0d56619ec4099a62b625f000c39983e5103d78f
AbdeI1 <abdelrahmanmmokbel@gmail.com>
2024-01-30 22:39:33 -0600

    all folders

---
+++ 2023/Day 10/code.py
@@ -0,0 +1,17 @@
+import pathlib
+from collections import Counter
+
+def reader():
+  f = open(f"{pathlib.Path(__file__).parent.resolve()}/sample.txt", 'r').read()
+  f = f.split('\n')
+  f = f[:-1]
+  return f
+
+def part1():
+  f = reader()
+  
+def part2():
+  f = reader()
+
+part1()
+part2()
