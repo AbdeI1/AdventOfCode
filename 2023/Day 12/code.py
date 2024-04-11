7546164766a8e9fb920ae701fbd290a08bd4cd66
AbdeI1 <abdelrahmanmmokbel@gmail.com>
2024-04-11 02:24:07 -0500

    day 12 finished

--- 2023/Day 12/code.py
+++ 2023/Day 12/code.py
@@ -4,7 +4,7 @@ from functools import cache
 
 
 def reader():
-  f = open(f"{pathlib.Path(__file__).parent.resolve()}/sample.txt", 'r').read()
+  f = open(f"{pathlib.Path(__file__).parent.resolve()}/input.txt", 'r').read()
   f = f.split('\n')
   f = f[:-1]
   return f
@@ -16,7 +16,7 @@ def part1():
   ans = 0
   for s, n in f:
     p = s.replace('.', 'a').replace('#', 'b').replace('?', '.')
-    diff = len(s) - sum(n) - len(n) + 1
+    diff = len(p) - sum(n) - len(n) + 1
     l = [0] * (len(n) + 1)
     t = 0
 
@@ -45,29 +45,37 @@ def part2():
        for x in reader()]
   ans = 0
   for s, n in f:
-    p = s.replace('.', 'a').replace('#', 'b').replace('?', '.')
-    diff = len(s) - sum(n) - len(n) + 1
-    l = [0] * (len(n) + 1)
+    rep = 5
+    s = '?'.join([s for _ in range(rep)])
+    n = n * rep
 
     @cache
-    def count(i, c):
-      if i == len(l) - 1:
-        l[i] = c
-        p2 = "a" * l[0]
-        for j in range(len(n) - 1):
-          p2 += "b" * n[j] + "a"
-          p2 += "a" * l[j + 1]
-        p2 += "b" * n[-1] + "a" * l[-1]
-        if re.fullmatch(p, p2):
-          return 1
-        return 0
-      else:
-        s = 0
-        for j in range(c + 1):
-          l[i] = j
-          s += count(i + 1, c - j)
-        return s
-    t = count(0, diff)
+    def count(i, j):
+      if i >= len(s):
+        return 1 if j >= len(n) else 0
+      if j >= len(n):
+        return count(i + 1, j) if s[i] in {'.', '?'} else 0
+      match s[i]:
+        case '.':
+          return count(i + 1, j)
+        case '#':
+          c2 = 0
+          c = 0
+          while c < n[j] and i + c < len(s) and s[i + c] in {'#', '?'}:
+            c += 1
+          if c == n[j] and (i + c >= len(s) or s[i + c] in {'.', '?'}):
+            c2 = count(i + c + 1, j + 1)
+          return c2
+        case '?':
+          c1 = count(i + 1, j)
+          c2 = 0
+          c = 0
+          while c < n[j] and i + c < len(s) and s[i + c] in {'#', '?'}:
+            c += 1
+          if c == n[j] and (i + c >= len(s) or s[i + c] in {'.', '?'}):
+            c2 = count(i + c + 1, j + 1)
+          return c1 + c2
+    t = count(0, 0)
     ans += t
   print(ans)
 
