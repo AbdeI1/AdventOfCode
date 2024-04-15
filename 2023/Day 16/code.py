e4ee49ef634775396e0be00e1db6dc35ffd65f6d
AbdeI1 <abdelrahmanmmokbel@gmail.com>
2024-04-15 18:44:55 -0500

    day 16

--- 2023/Day 16/code.py
+++ 2023/Day 16/code.py
@@ -1,16 +1,145 @@
 import pathlib
 
+
 def reader():
-  f = open(f"{pathlib.Path(__file__).parent.resolve()}/sample.txt", 'r').read()
+  f = open(f"{pathlib.Path(__file__).parent.resolve()}/input.txt", 'r').read()
   f = f.split('\n')
   f = f[:-1]
   return f
 
+
 def part1():
   f = reader()
-  
+  v = set()
+
+  s = [(0, 0, 'e')]
+
+  while s:
+    i, j, d = s.pop()
+    if i not in range(len(f)) or j not in range(len(f[i])):
+      continue
+    if (i, j, d) in v:
+      continue
+    v.add((i, j, d))
+    match d:
+      case 'n':
+        match f[i][j]:
+          case '\\':
+            s.append((i, j - 1, 'w'))
+          case '/':
+            s.append((i, j + 1, 'e'))
+          case '-':
+            s.append((i, j + 1, 'e'))
+            s.append((i, j - 1, 'w'))
+          case _:
+            s.append((i - 1, j, 'n'))
+      case 'e':
+        match f[i][j]:
+          case '\\':
+            s.append((i + 1, j, 's'))
+          case '/':
+            s.append((i - 1, j, 'n'))
+          case '|':
+            s.append((i - 1, j, 'n'))
+            s.append((i + 1, j, 's'))
+          case _:
+            s.append((i, j + 1, 'e'))
+      case 's':
+        match f[i][j]:
+          case '\\':
+            s.append((i, j + 1, 'e'))
+          case '/':
+            s.append((i, j - 1, 'w'))
+          case '-':
+            s.append((i, j + 1, 'e'))
+            s.append((i, j - 1, 'w'))
+          case _:
+            s.append((i + 1, j, 's'))
+      case 'w':
+        match f[i][j]:
+          case '\\':
+            s.append((i - 1, j, 'n'))
+          case '/':
+            s.append((i + 1, j, 's'))
+          case '|':
+            s.append((i - 1, j, 'n'))
+            s.append((i + 1, j, 's'))
+          case _:
+            s.append((i, j - 1, 'w'))
+
+  print(len({(i, j) for (i, j, d) in v}))
+
+
 def part2():
   f = reader()
+
+  def simulate(start):
+    v = set()
+    s = [start]
+
+    while s:
+      i, j, d = s.pop()
+      if i not in range(len(f)) or j not in range(len(f[i])):
+        continue
+      if (i, j, d) in v:
+        continue
+      v.add((i, j, d))
+      match d:
+        case 'n':
+          match f[i][j]:
+            case '\\':
+              s.append((i, j - 1, 'w'))
+            case '/':
+              s.append((i, j + 1, 'e'))
+            case '-':
+              s.append((i, j + 1, 'e'))
+              s.append((i, j - 1, 'w'))
+            case _:
+              s.append((i - 1, j, 'n'))
+        case 'e':
+          match f[i][j]:
+            case '\\':
+              s.append((i + 1, j, 's'))
+            case '/':
+              s.append((i - 1, j, 'n'))
+            case '|':
+              s.append((i - 1, j, 'n'))
+              s.append((i + 1, j, 's'))
+            case _:
+              s.append((i, j + 1, 'e'))
+        case 's':
+          match f[i][j]:
+            case '\\':
+              s.append((i, j + 1, 'e'))
+            case '/':
+              s.append((i, j - 1, 'w'))
+            case '-':
+              s.append((i, j + 1, 'e'))
+              s.append((i, j - 1, 'w'))
+            case _:
+              s.append((i + 1, j, 's'))
+        case 'w':
+          match f[i][j]:
+            case '\\':
+              s.append((i - 1, j, 'n'))
+            case '/':
+              s.append((i + 1, j, 's'))
+            case '|':
+              s.append((i - 1, j, 'n'))
+              s.append((i + 1, j, 's'))
+            case _:
+              s.append((i, j - 1, 'w'))
+    return len({(i, j) for (i, j, _) in v})
+
+  m = -1
+
+  for i in range(len(f)):
+    m = max(m, simulate((i, 0, 'e')), simulate((i, len(f[i]) - 1, 'w')))
+  for j in range(len(f[0])):
+    m = max(m, simulate((0, j, 's')), simulate((len(f[0]) - 1, j, 'n')))
+
+  print(m)
+
 
 part1()
 part2()
