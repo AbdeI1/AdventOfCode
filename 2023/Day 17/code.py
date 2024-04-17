d5b40af594582e9734b2670d06c62112b3f3a792
AbdeI1 <abdelrahmanmmokbel@gmail.com>
2024-04-16 22:23:49 -0500

    day 17

--- 2023/Day 17/code.py
+++ 2023/Day 17/code.py
@@ -1,16 +1,85 @@
 import pathlib
+from heapq import *
+
 
 def reader():
-  f = open(f"{pathlib.Path(__file__).parent.resolve()}/sample.txt", 'r').read()
+  f = open(f"{pathlib.Path(__file__).parent.resolve()}/input.txt", 'r').read()
   f = f.split('\n')
   f = f[:-1]
   return f
 
+
 def part1():
-  f = reader()
-  
+  f = [[int(c) for c in s] for s in reader()]
+  q = [(-f[0][0], (0, 0, 'e', 0))]
+  v = set()
+  while q:
+    hl, (i, j, d, s) = heappop(q)
+    if i not in range(len(f)) or j not in range(len(f[i])) or s > 3:
+      continue
+    if (i, j, d, s) in v:
+      continue
+    v.add((i, j, d, s))
+    hl += f[i][j]
+    if (i, j) == (len(f) - 1, len(f[i]) - 1):
+      print(hl)
+      break
+    match d:
+      case 'n':
+        heappush(q, (hl, (i - 1, j, 'n', s + 1)))
+        heappush(q, (hl, (i, j + 1, 'e', 1)))
+        heappush(q, (hl, (i, j - 1, 'w', 1)))
+      case 'e':
+        heappush(q, (hl, (i, j + 1, 'e', s + 1)))
+        heappush(q, (hl, (i - 1, j, 'n', 1)))
+        heappush(q, (hl, (i + 1, j, 's', 1)))
+      case 's':
+        heappush(q, (hl, (i + 1, j, 's', s + 1)))
+        heappush(q, (hl, (i, j + 1, 'e', 1)))
+        heappush(q, (hl, (i, j - 1, 'w', 1)))
+      case 'w':
+        heappush(q, (hl, (i, j - 1, 'w', s + 1)))
+        heappush(q, (hl, (i - 1, j, 'n', 1)))
+        heappush(q, (hl, (i + 1, j, 's', 1)))
+
+
 def part2():
-  f = reader()
+  f = [[int(c) for c in s] for s in reader()]
+  q = [(-f[0][0], (0, 0, 'e', 0))]
+  v = set()
+  while q:
+    hl, (i, j, d, s) = heappop(q)
+    if i not in range(len(f)) or j not in range(len(f[i])) or s > 10:
+      continue
+    if (i, j, d, s) in v:
+      continue
+    v.add((i, j, d, s))
+    hl += f[i][j]
+    if (i, j) == (len(f) - 1, len(f[i]) - 1) and s > 3:
+      print(hl)
+      break
+    match d:
+      case 'n':
+        heappush(q, (hl, (i - 1, j, 'n', s + 1)))
+        if s >= 4:
+          heappush(q, (hl, (i, j + 1, 'e', 1)))
+          heappush(q, (hl, (i, j - 1, 'w', 1)))
+      case 'e':
+        heappush(q, (hl, (i, j + 1, 'e', s + 1)))
+        if s >= 4:
+          heappush(q, (hl, (i - 1, j, 'n', 1)))
+          heappush(q, (hl, (i + 1, j, 's', 1)))
+      case 's':
+        heappush(q, (hl, (i + 1, j, 's', s + 1)))
+        if s >= 4:
+          heappush(q, (hl, (i, j + 1, 'e', 1)))
+          heappush(q, (hl, (i, j - 1, 'w', 1)))
+      case 'w':
+        heappush(q, (hl, (i, j - 1, 'w', s + 1)))
+        if s >= 4:
+          heappush(q, (hl, (i - 1, j, 'n', 1)))
+          heappush(q, (hl, (i + 1, j, 's', 1)))
+
 
 part1()
 part2()
