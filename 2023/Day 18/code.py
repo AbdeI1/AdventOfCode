4fad3cb8ae2b5eee64cc721dfd3c5ce0fe8f3317
AbdeI1 <abdelrahmanmmokbel@gmail.com>
2024-04-18 09:39:38 -0500

    day 18

--- 2023/Day 18/code.py
+++ 2023/Day 18/code.py
@@ -3,7 +3,7 @@ from itertools import pairwise
 
 
 def reader():
-  f = open(f"{pathlib.Path(__file__).parent.resolve()}/sample.txt", 'r').read()
+  f = open(f"{pathlib.Path(__file__).parent.resolve()}/input.txt", 'r').read()
   f = f.split('\n')
   f = f[:-1]
   return f
@@ -30,13 +30,31 @@ def part1():
   for p1, p2 in pairwise(coords):
     a += (p1[0] * p2[1] - p2[0] * p1[1])
   a = abs(a) // 2
-  print(a)
-  print(p)
-  print(coords)
+  print(a + (p // 2) + 1)
 
 
 def part2():
-  f = reader()
+  f = [({'0': 'R', '1': 'D', '2': 'L', '3': 'U'}[c[-2]], int(c[2:-2], 16), int(c[2:-1], 16))
+       for _, _, c in [s.split() for s in reader()]]
+  p = sum([n for _, n, _ in f])
+  x, y = 0, 0
+  coords = [(x, y)]
+  for d, n, _ in f:
+    match d:
+      case 'R':
+        x += n
+      case 'L':
+        x -= n
+      case 'D':
+        y -= n
+      case 'U':
+        y += n
+    coords.append((x, y))
+  a = 0
+  for p1, p2 in pairwise(coords):
+    a += (p1[0] * p2[1] - p2[0] * p1[1])
+  a = abs(a) // 2
+  print(a + (p // 2) + 1)
 
 
 part1()
