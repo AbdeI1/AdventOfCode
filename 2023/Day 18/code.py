463d2d51ffb7b847bcc4d730847fed05d3da6d70
AbdeI1 <abdelrahmanmmokbel@gmail.com>
2024-04-18 03:23:49 -0500

    day 18 attempts

--- 2023/Day 18/code.py
+++ 2023/Day 18/code.py
@@ -1,4 +1,6 @@
 import pathlib
+from itertools import pairwise
+
 
 def reader():
   f = open(f"{pathlib.Path(__file__).parent.resolve()}/sample.txt", 'r').read()
@@ -6,11 +8,36 @@ def reader():
   f = f[:-1]
   return f
 
+
 def part1():
-  f = reader()
-  
+  f = [(d, int(n), int(c[2:-1], 16))
+       for d, n, c in [s.split() for s in reader()]]
+  p = sum([n for _, n, _ in f])
+  x, y = 0, 0
+  coords = [(x, y)]
+  for d, n, _ in f:
+    match d:
+      case 'R':
+        x += n
+      case 'L':
+        x -= n
+      case 'D':
+        y -= n
+      case 'U':
+        y += n
+    coords.append((x, y))
+  a = 0
+  for p1, p2 in pairwise(coords):
+    a += (p1[0] * p2[1] - p2[0] * p1[1])
+  a = abs(a) // 2
+  print(a)
+  print(p)
+  print(coords)
+
+
 def part2():
   f = reader()
+
 
 part1()
 part2()
