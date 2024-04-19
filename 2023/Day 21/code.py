30c96f02cf5d9693904c578c6d37e17508f408c1
AbdeI1 <abdelrahmanmmokbel@gmail.com>
2024-04-19 00:08:02 -0500

    day 21 p1

--- 2023/Day 21/code.py
+++ 2023/Day 21/code.py
@@ -1,16 +1,70 @@
 import pathlib
 
+
 def reader():
   f = open(f"{pathlib.Path(__file__).parent.resolve()}/sample.txt", 'r').read()
   f = f.split('\n')
   f = f[:-1]
   return f
 
+
 def part1():
   f = reader()
-  
+  d = 64
+  s = (0, 0)
+  for i in range(len(f)):
+    for j in range(len(f[i])):
+      if f[i][j] == 'S':
+        s = (i, j)
+        break
+    else:
+      continue
+    break
+  q = [(s, 0)]
+  v = set()
+  ans = 0
+  while q:
+    (i, j), s = q.pop(0)
+    if s > d or i not in range(len(f)) or j not in range(len(f[i])) or f[i][j] == '#':
+      continue
+    if (i, j) in v:
+      continue
+    v.add((i, j))
+    if s % 2 == 0:
+      ans += 1
+    for d1, d2 in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+      q.append(((i + d1, j + d2), s + 1))
+  print(ans)
+
+
 def part2():
   f = reader()
+  d = 100
+  s = (0, 0)
+  for i in range(len(f)):
+    for j in range(len(f[i])):
+      if f[i][j] == 'S':
+        s = (i, j)
+        break
+    else:
+      continue
+    break
+  q = [(s, 0)]
+  v = set()
+  ans = 0
+  while q:
+    (i, j), s = q.pop(0)
+    if s > d or f[i % len(f)][j % len(f[i % len(f)])] == '#':
+      continue
+    if (i, j) in v:
+      continue
+    v.add((i, j))
+    if s % 2 == d % 2:
+      ans += 1
+    for d1, d2 in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+      q.append(((i + d1, j + d2), s + 1))
+  print(ans)
+
 
 part1()
 part2()
