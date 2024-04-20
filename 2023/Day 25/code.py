ae798215d09bebc8a9d8ff58d6b64dc2c6216dcb
AbdeI1 <abdelrahmanmmokbel@gmail.com>
2024-04-20 13:19:58 -0500

    day 25

--- 2023/Day 25/code.py
+++ 2023/Day 25/code.py
@@ -1,16 +1,61 @@
 import pathlib
 
+
 def reader():
-  f = open(f"{pathlib.Path(__file__).parent.resolve()}/sample.txt", 'r').read()
+  f = open(f"{pathlib.Path(__file__).parent.resolve()}/input.txt", 'r').read()
   f = f.split('\n')
   f = f[:-1]
   return f
 
+
 def part1():
   f = reader()
-  
-def part2():
-  f = reader()
+  g = {}
+  for l in f:
+    n, o = l.split(": ")
+    o = o.split(" ")
+    if n not in g:
+      g[n] = set()
+    for n2 in o:
+      if n2 not in g:
+        g[n2] = set()
+      g[n].add(n2)
+      g[n2].add(n)
+
+  def num_paths(a, b):
+    e = set()
+    count = 0
+    for n in g[a]:
+      s = [(n, a)]
+      v = {a}
+      p = {}
+      while s:
+        o, op = s.pop(0)
+        if o in v:
+          continue
+        v.add(o)
+        p[o] = op
+        if o == b:
+          count += 1
+          break
+        for n2 in g[o]:
+          if (o, n2) not in e and (n2, o) not in e:
+            s.append((n2, o))
+      else:
+        continue
+      x = b
+      while x != a:
+        e.add((x, p[x]))
+        e.add((p[x], x))
+        x = p[x]
+    return count
+
+  o = list(g)[0]
+  n = 0
+  for a in g:
+    if num_paths(o, a) == 3:
+      n += 1
+  print(n * (len(g) - n))
+
 
 part1()
-part2()
