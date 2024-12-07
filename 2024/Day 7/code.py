a23acd5a534072b36ecd99984f27ddc573f44c01
AbdeI1 <abdelrahmanmmokbel@gmail.com>
2024-12-06 23:26:46 -0600

    day 7

--- 2024/Day 7/code.py
+++ 2024/Day 7/code.py
@@ -6,11 +6,44 @@ def reader():
 
 
 def part1():
-  pass
+  t = 0
+  for l in reader():
+    r, n = l.split(": ")
+    r = int(r)
+    n = list(map(int, n.split(" ")))
+    for i in range(1 << (len(n) - 1)):
+      x = n[0]
+      for b in range(len(n) - 1):
+        if ((i >> b) & 1):
+          x += n[b + 1]
+        else:
+          x *= n[b + 1]
+      if x == r:
+        t += r
+        break
+  print(t)
 
 
 def part2():
-  pass
+  t = 0
+  for l in reader():
+    r, n = l.split(": ")
+    r = int(r)
+    n = list(map(int, n.split(" ")))
+    for i in range(3 ** (len(n) - 1)):
+      x = n[0]
+      for b in range(len(n) - 1):
+        a = (i // (3 ** b))
+        if a % 3 == 0:
+          x += n[b + 1]
+        elif a % 3 == 1:
+          x *= n[b + 1]
+        else:
+          x = int(str(x) + str(n[b + 1]))
+      if x == r:
+        t += r
+        break
+  print(t)
 
 
 part1()
