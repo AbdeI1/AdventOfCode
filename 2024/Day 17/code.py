13e8fb8a60fde6773304ce27d4a896f37b096f8e
AbdeI1 <abdelrahmanmmokbel@gmail.com>
2024-12-17 21:35:57 -0600

    day 17

--- 2024/Day 17/code.py
+++ 2024/Day 17/code.py
@@ -5,12 +5,60 @@ def reader():
   return open(f"{pathlib.Path(__file__).parent.resolve()}/input.txt", 'r').read().split('\n')[:-1]
 
 
+def simulate(program, A):
+  l = [0, 1, 2, 3, A, 0, 0, -1]
+  out = []
+  i = 0
+  while i < len(program):
+    op = program[i]
+    operand = program[i + 1]
+    if op == 0:
+      l[4] = int(l[4] / (2 ** l[operand]))
+    elif op == 1:
+      l[5] = l[5] ^ operand
+    elif op == 2:
+      l[5] = l[operand] % 8
+    elif op == 3:
+      if l[4] != 0:
+        i = operand
+        continue
+    elif op == 4:
+      l[5] = l[5] ^ l[6]
+    elif op == 5:
+      out.append(l[operand] % 8)
+    elif op == 6:
+      l[5] = int(l[4] / (2 ** l[operand]))
+    elif op == 7:
+      l[6] = int(l[4] / (2 ** l[operand]))
+    i += 2
+  return out
+
+
 def part1():
-  pass
+  f = reader()
+  program = list(map(int, f[4][(f[4].find(': ') + 2):].split(',')))
+  print(
+    ','.join(map(str, simulate(program, int(f[0][(f[0].find(': ') + 2):])))))
 
 
 def part2():
-  pass
+  f = reader()
+  program = list(map(int, f[4][(f[4].find(': ') + 2):].split(',')))
+
+  S = set()
+
+  def backtrack(A, j):
+    if -j > len(program):
+      S.add(A >> 3)
+      return
+    for i in range(8):
+      s = (i ^ 0b101)
+      t = A | i | ((program[j] ^ 0b011 ^ i) << s)
+      if simulate(program, t)[j:] == program[j:]:
+        backtrack(t << 3, j - 1)
+  backtrack(0, -1)
+
+  print(min(S))
 
 
 part1()
