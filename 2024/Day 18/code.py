3164e073ee45de43f974ba011c1651cf32c69f4c
AbdeI1 <abdelrahmanmmokbel@gmail.com>
2024-12-17 23:12:24 -0600

    initial day 18

--- 2024/Day 18/code.py
+++ 2024/Day 18/code.py
@@ -6,11 +6,57 @@ def reader():
 
 
 def part1():
-  pass
+  f = list(map(eval, reader()))
+  W, H = 71, 71
+  G = [['.' for _ in range(W)] for _ in range(H)]
+  for x, y in f[:1024]:
+    G[y][x] = '#'
+  D = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+  q = [(0, 0, 0)]
+  V = set()
+  while q:
+    d, x, y = q.pop(0)
+    if (x, y) in V:
+      continue
+    V.add((x, y))
+    if (x, y) == (W - 1, H - 1):
+      print(d)
+      break
+    for dx, dy in D:
+      xx, yy = x + dx, y + dy
+      if yy in range(len(G)) and xx in range(len(G[yy])) and G[yy][xx] == '.':
+        q.append((d + 1, xx, yy))
 
 
 def part2():
-  pass
+  f = list(map(eval, reader()))
+  W, H = 71, 71
+  G = [['.' for _ in range(W)] for _ in range(H)]
+  D = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+  def bfs():
+    q = [(0, 0, 0)]
+    V = set()
+    while q:
+      d, x, y = q.pop(0)
+      if (x, y) in V:
+        continue
+      V.add((x, y))
+      if (x, y) == (W - 1, H - 1):
+        return True
+      for dx, dy in D:
+        xx, yy = x + dx, y + dy
+        if yy in range(len(G)) and xx in range(len(G[yy])) and G[yy][xx] == '.':
+          q.append((d + 1, xx, yy))
+    return False
+
+  n = len(f)
+
+  for x, y in f:
+    G[x][y] = '#'
+    if not bfs():
+      print(x, y)
+      break
 
 
 part1()
