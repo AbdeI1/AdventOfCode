20318005e4eff447032f34ef50b69d2e0ebabc4a
AbdeI1 <abdelrahmanmmokbel@gmail.com>
2024-12-18 23:45:50 -0600

    day 18 union-find

--- 2024/Day 18/code.py
+++ 2024/Day 18/code.py
@@ -1,5 +1,6 @@
 import pathlib
 import math
+import time
 
 
 def reader():
@@ -30,6 +31,42 @@ def part1():
 
 
 def part2():
+  f = list(map(eval, reader()))
+  W, H = 71, 71
+  G = [['.' for _ in range(W)] for _ in range(H)]
+  for x, y in f:
+    G[y][x] = '#'
+  P = [[(x, y) for x in range(W)] for y in range(H)]
+
+  def find(x, y):
+    return P[y][x] if P[y][x] == (x, y) else find(*P[y][x])
+
+  def union(x1, y1, x2, y2):
+    p1, p2 = find(x1, y1), find(x2, y2)
+    P[p1[1]][p1[0]] = p2
+
+  D = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+  for y in range(H):
+    for x in range(W):
+      if G[y][x] == '#':
+        continue
+      for dx, dy in D:
+        xx, yy = x + dx, y + dy
+        if yy in range(len(G)) and xx in range(len(G[yy])) and G[yy][xx] == '.':
+          union(x, y, xx, yy)
+
+  for x, y in f[::-1]:
+    G[y][x] = '.'
+    for dx, dy in D:
+      xx, yy = x + dx, y + dy
+      if yy in range(len(G)) and xx in range(len(G[yy])) and G[yy][xx] == '.':
+        union(x, y, xx, yy)
+    if find(0, 0) == find(W - 1, H - 1):
+      print(','.join(map(str, (x, y))))
+      break
+
+
+def original_part2():
   f = list(map(eval, reader()))
   W, H = 71, 71
   G = [['.' for _ in range(W)] for _ in range(H)]
