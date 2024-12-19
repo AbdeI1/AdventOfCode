d7c4e4c941f6a416cde985e451dd150bbe07352a
AbdeI1 <abdelrahmanmmokbel@gmail.com>
2024-12-18 23:14:15 -0600

    day 19

--- 2024/Day 19/code.py
+++ 2024/Day 19/code.py
@@ -1,4 +1,5 @@
 import pathlib
+from functools import cache
 
 
 def reader():
@@ -6,11 +7,48 @@ def reader():
 
 
 def part1():
-  pass
+  patterns, designs = '\n'.join(reader()).split('\n\n')
+  patterns = patterns.split(', ')
+  designs = designs.split('\n')
+
+  @cache
+  def test(d, i):
+    if i >= len(d):
+      return True
+    b = False
+    for p in patterns:
+      if d[i:i + len(p)] == p:
+        b = b or test(d, i + len(p))
+    return b
+
+  c = 0
+  for d in designs:
+    if test(d, 0):
+      c += 1
+
+  print(c)
 
 
 def part2():
-  pass
+  patterns, designs = '\n'.join(reader()).split('\n\n')
+  patterns = patterns.split(', ')
+  designs = designs.split('\n')
+
+  @cache
+  def test(d, i):
+    if i >= len(d):
+      return 1
+    c = 0
+    for p in patterns:
+      if d[i:i + len(p)] == p:
+        c += test(d, i + len(p))
+    return c
+
+  c = 0
+  for d in designs:
+    c += test(d, 0)
+
+  print(c)
 
 
 part1()
