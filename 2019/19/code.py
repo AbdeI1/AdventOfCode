6f64decceab5d894fdcb85abc642348bcf1895b4
AbdeI1 <abdelrahmanmmokbel@gmail.com>
2024-12-27 00:04:08 -0600

    template files

---
+++ 2019/19/code.py
@@ -0,0 +1,18 @@
+import os
+os.chdir(os.path.dirname(__file__))
+
+
+def reader():
+  return open(f"input.txt", 'r').read().split('\n')[:-1]
+
+
+def part1():
+  pass
+
+
+def part2():
+  pass
+
+
+part1()
+part2()
--- fetcher.py
+++ fetcher.py
@@ -1,18 +1,53 @@
 import os
 os.chdir(os.path.dirname(__file__))
+from aocd import get_data, submit
 
 years = range(2015, 2020)
 days = range(1, 26)
 
-for y in years:
-  yname = f"{y}"
-  if not os.path.exists(yname):
-    os.mkdir(yname)
-  os.chdir(yname)
-  for d in days:
-    dname = f"{d:02}"
-    if not os.path.exists(dname):
-      os.mkdir(dname)
-    os.chdir(dname)
+
+def yname(y): return f"{y}"
+def dname(d): return f"{d:02}"
+
+
+def init(years, days):
+  for y in years:
+    if not os.path.exists(yname(y)):
+      os.mkdir(yname(y))
+    os.chdir(yname(y))
+    for d in days:
+      if not os.path.exists(dname(d)):
+        os.mkdir(dname(d))
+      os.chdir(dname(d))
+      if not os.path.exists('code.py'):
+        with open('code.py', 'w') as f:
+          f.write("""import os
+os.chdir(os.path.dirname(__file__))
+
+
+def reader():
+  return open(f"input.txt", 'r').read().split('\\n')[:-1]
+
+
+def part1():
+  pass
+
+
+def part2():
+  pass
+
+
+part1()
+part2()
+""")
+      os.chdir('..')
     os.chdir('..')
-  os.chdir('..')
+
+
+def fetch(year, day):
+  os.chdir(f"{yname(year)}/{dname(day)}")
+  with open('input.txt', 'w') as f:
+    f.write(get_data(year=year, day=day) + '\n')
+
+
+init(years, days)
