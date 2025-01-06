cc600ff529aaa85281885c22def09d0c0556719b
AbdeI1 <abdelrahmanmmokbel@gmail.com>
2025-01-06 17:47:23 -0600

    2019 16

--- 2019/16/code.py
+++ 2019/16/code.py
@@ -1,7 +1,7 @@
 import os
 os.chdir(os.path.dirname(__file__))
 import numpy as np
-from functools import cache
+from itertools import accumulate
 
 
 def reader():
@@ -9,7 +9,7 @@ def reader():
 
 
 def part1():
-  l = list(map(int, reader()[0] * 4))
+  l = list(map(int, reader()[0]))
   T = [[0 for _ in range(len(l))] for _ in range(len(l))]
   for i in range(len(l)):
     for j in range(len(l)):
@@ -17,18 +17,10 @@ def part1():
 
   T = np.array(T)
   l = np.array(l)
-  np.set_printoptions(threshold=np.inf)
-
-  print(T)
 
   def fft(l): return abs(T @ l) % 10
 
-  print(l)
-  print(fft(l))
-  print(fft(fft(l)))
-  print(fft(fft(fft(l))))
-
-  for _ in range(1):
+  for _ in range(100):
     l = fft(l)
 
   print(''.join(map(str, l[:8])))
@@ -38,29 +30,9 @@ def part2():
   n = list(map(int, reader()[0] * 10000))
   o = int(''.join(map(str, n[:7])))
 
-  @cache
-  def dig(l, i):
-    if l == 0:
-      return n[i % len(n)]
-    x, m = 0, -1
-    for y in range(-1, len(n), i + 1):
-      m = (m + 1) % 4
-      if m % 2 == 0:
-        continue
-      t = sum([dig(l - 1, j) for j in range(y, min(len(n), y + i + 1))])
-      if m == 1:
-        x += t
-      else:
-        x -= t
-    return abs(x) % 10
-
-  # print(dig(1, 651))
-  # o = 0
-  # print(''.join(map(str, [dig(100, i) for i in range(o, o + 8)])))
+  print(''.join(map(str, (lambda v: [v := [abs(x) % 10 for x in list(accumulate(v[::-1]))[::-1]]
+                                     for _ in range(100)])(n[o:])[-1][:8])))
 
 
 part1()
-# part2()
-
-5971873
-65100000
+part2()
