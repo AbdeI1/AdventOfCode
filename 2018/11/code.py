e4e4a404d43fffa136f7d866b525ee5e6508cb99
AbdeI1 <abdelrahmanmmokbel@gmail.com>
2025-01-31 15:03:05 -0600

    2018, 12

--- 2018/11/code.py
+++ 2018/11/code.py
@@ -8,11 +8,43 @@ def reader():
 
 def part1():
   n = int(reader()[0])
-  print(n)
+  G = [[int(f'{((x + 10) * y + n) * (x + 10):03}'[-3]) -
+        5 for x in range(1, 301)] for y in range(1, 301)]
+  Gp = [[0] * 301 for _ in range(301)]
+  for y in range(1, 301):
+    for x in range(1, 301):
+      Gp[y][x] = Gp[y - 1][x] + Gp[y][x - 1] - \
+        Gp[y - 1][x - 1] + G[y - 1][x - 1]
+  m = -float('inf')
+  r = (-1, -1)
+  for y in range(1, 298):
+    for x in range(1, 298):
+      s = Gp[y + 3][x + 3] - Gp[y + 3][x] - Gp[y][x + 3] + Gp[y][x]
+      if s > m:
+        m = s
+        r = (x + 1, y + 1)
+  print(','.join(map(str, r)))
 
 
 def part2():
-  pass
+  n = int(reader()[0])
+  G = [[int(f'{((x + 10) * y + n) * (x + 10):03}'[-3]) -
+        5 for x in range(1, 301)] for y in range(1, 301)]
+  Gp = [[0] * 301 for _ in range(301)]
+  for y in range(1, 301):
+    for x in range(1, 301):
+      Gp[y][x] = Gp[y - 1][x] + Gp[y][x - 1] - \
+        Gp[y - 1][x - 1] + G[y - 1][x - 1]
+  m = -float('inf')
+  r = (-1, -1, -1)
+  for y in range(1, 301):
+    for x in range(1, 301):
+      for s in range(1, 301 - max(x, y)):
+        p = Gp[y + s][x + s] - Gp[y + s][x] - Gp[y][x + s] + Gp[y][x]
+        if p > m:
+          m = p
+          r = (x + 1, y + 1, s)
+  print(','.join(map(str, r)))
 
 
 part1()
--- 2018/12/code.py
+++ 2018/12/code.py
@@ -1,5 +1,6 @@
 import os
 os.chdir(os.path.dirname(__file__))
+from collections import defaultdict
 
 
 def reader():
@@ -7,11 +8,44 @@ def reader():
 
 
 def part1():
-  pass
+  f = reader()
+  s = f[0].split(": ")[1]
+  s = defaultdict(int, {i: 1 for i, c in enumerate(s) if c == '#'})
+  R = [0] * 32
+  for i in f[2:]:
+    a, b = i.replace('#', '1').replace('.', '0').split(" => ")
+    R[int(a[::-1], 2)] = int(b)
+  for _ in range(20):
+    sn = defaultdict(int)
+    for i in range(min(s.keys()) - 2, max(s.keys()) + 2):
+      l = sum(2 ** j for j in range(5) if s[i - 2 + j])
+      if R[l]:
+        sn[i] = R[l]
+    s = sn
+  print(sum(s.keys()))
 
 
 def part2():
-  pass
+  f = reader()
+  s = f[0].split(": ")[1]
+  s = {i for i, c in enumerate(s) if c == '#'}
+  R = [0] * 32
+  for i in f[2:]:
+    a, b = i.replace('#', '1').replace('.', '0').split(" => ")
+    R[int(a[::-1], 2)] = int(b)
+  N = 50000000000
+  t = 0
+  while True:
+    sn = set()
+    for i in range(min(s) - 2, max(s) + 2):
+      l = sum(2 ** j for j in range(5) if i - 2 + j in s)
+      if R[l]:
+        sn.add(i)
+    if len(sn) == len(s) and len({x - y for x, y in zip(sorted(sn), sorted(s))}) == 1:
+      break
+    s = sn
+    t += 1
+  print(sum(s) + (N - t) * len(s))
 
 
 part1()
