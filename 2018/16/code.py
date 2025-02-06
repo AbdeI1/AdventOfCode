21f3327b8954d2c74aab6aff7fbcec281551796c
AbdeI1 <abdelrahmanmmokbel@gmail.com>
2025-02-05 18:45:02 -0600

    2018 16 17

--- 2018/16/code.py
+++ 2018/16/code.py
@@ -6,12 +6,117 @@ def reader():
   return open(f"input.txt", 'r').read().splitlines()
 
 
+def addr(R, A, B, C):
+  R[C] = R[A] + R[B]
+
+
+def addi(R, A, B, C):
+  R[C] = R[A] + B
+
+
+def mulr(R, A, B, C):
+  R[C] = R[A] * R[B]
+
+
+def muli(R, A, B, C):
+  R[C] = R[A] * B
+
+
+def banr(R, A, B, C):
+  R[C] = R[A] & R[B]
+
+
+def bani(R, A, B, C):
+  R[C] = R[A] & B
+
+
+def borr(R, A, B, C):
+  R[C] = R[A] | R[B]
+
+
+def bori(R, A, B, C):
+  R[C] = R[A] | B
+
+
+def setr(R, A, B, C):
+  R[C] = R[A]
+
+
+def seti(R, A, B, C):
+  R[C] = A
+
+
+def gtir(R, A, B, C):
+  R[C] = 1 if A > R[B] else 0
+
+
+def gtri(R, A, B, C):
+  R[C] = 1 if R[A] > B else 0
+
+
+def gtrr(R, A, B, C):
+  R[C] = 1 if R[A] > R[B] else 0
+
+
+def eqir(R, A, B, C):
+  R[C] = 1 if A == R[B] else 0
+
+
+def eqri(R, A, B, C):
+  R[C] = 1 if R[A] == B else 0
+
+
+def eqrr(R, A, B, C):
+  R[C] = 1 if R[A] == R[B] else 0
+
+
+OPs = [addr, addi, mulr, muli, banr, bani, borr, bori,
+       setr, seti, gtir, gtri, gtrr, eqir, eqri, eqrr]
+
+
 def part1():
-  pass
+  f, p = '\n'.join(reader()).split('\n\n\n\n')
+  f = [(lambda a, b, c: [tuple(eval(a[a.find(': ') + 2:])), tuple(map(int, b.split())),
+        tuple(eval(c[c.find(': ') + 2:]))])(*s.splitlines()) for s in f.split('\n\n')]
+  p = [tuple(map(int, l.split())) for l in p.splitlines()]
+  t = 0
+  for b, (_, A, B, C), a in f:
+    c = 0
+    for op in OPs:
+      R = list(b)
+      op(R, A, B, C)
+      if tuple(R) == a:
+        c += 1
+    if c >= 3:
+      t += 1
+  print(t)
 
 
 def part2():
-  pass
+  global OPs
+  f, p = '\n'.join(reader()).split('\n\n\n\n')
+  f = [(lambda a, b, c: [tuple(eval(a[a.find(': ') + 2:])), tuple(map(int, b.split())),
+        tuple(eval(c[c.find(': ') + 2:]))])(*s.splitlines()) for s in f.split('\n\n')]
+  p = [tuple(map(int, l.split())) for l in p.splitlines()]
+  M = [set(range(16)) for _ in range(16)]
+  for b, (o, A, B, C), a in f:
+    c = 0
+    for i, op in enumerate(OPs):
+      R = list(b)
+      op(R, A, B, C)
+      if tuple(R) != a:
+        M[o].discard(i)
+  while any(len(s) > 1 for s in M):
+    for i, s in enumerate(M):
+      if len(s) == 1:
+        for j, t in enumerate(M):
+          if i != j:
+            t -= s
+  OPs = [OPs[list(i)[0]] for i in M]
+  R = [0, 0, 0, 0]
+  for o, A, B, C in p:
+    OPs[o](R, A, B, C)
+  print(R[0])
 
 
 part1()
--- 2018/17/code.py
+++ 2018/17/code.py
@@ -1,5 +1,8 @@
 import os
 os.chdir(os.path.dirname(__file__))
+import sys
+sys.setrecursionlimit(10000)
+from functools import cache
 
 
 def reader():
@@ -7,11 +10,89 @@ def reader():
 
 
 def part1():
-  pass
+  f = [tuple(range(int(c[2:]), int(c[2:]) + 1) if '..' not in c else range(int(c[2:c.index('..')]), int(c[c.index('..') + 2:]) + 1)
+             for c in sorted(l.split(', '))) for l in reader()]
+  Ymm = min(ry.start for _, ry in f)
+  Ym = max(ry.stop for _, ry in f)
+
+  B = {(x, y) for xr, yr in f for x in xr for y in yr}
+  V = {}
+
+  def flow(x, y):
+    if (x, y) in V:
+      return V[(x, y)]
+    if (x, y) in B:
+      return 0
+    if y >= Ym:
+      return -1
+    d = flow(x, y + 1)
+    if d == -1:
+      V[(x, y)] = -1
+      return -1
+    l = x - 1
+    while (l, y) not in B:
+      V[(l, y)] = -1
+      if flow(l, y + 1) != 0:
+        break
+      l -= 1
+    r = x + 1
+    while (r, y) not in B:
+      V[(r, y)] = -1
+      if flow(r, y + 1) != 0:
+        break
+      r += 1
+    if (l, y) in B and (r, y) in B:
+      for xx in range(l + 1, r):
+        V[(xx, y)] = 0
+      return 0
+    V[(x, y)] = -1
+    return -1
+
+  flow(500, 0)
+  print(len([y for _, y in V if y in range(Ymm, Ym + 1)]))
 
 
 def part2():
-  pass
+  f = [tuple(range(int(c[2:]), int(c[2:]) + 1) if '..' not in c else range(int(c[2:c.index('..')]), int(c[c.index('..') + 2:]) + 1)
+             for c in sorted(l.split(', '))) for l in reader()]
+  Ymm = min(ry.start for _, ry in f)
+  Ym = max(ry.stop for _, ry in f)
+
+  B = {(x, y) for xr, yr in f for x in xr for y in yr}
+  V = {}
+
+  def flow(x, y):
+    if (x, y) in V:
+      return V[(x, y)]
+    if (x, y) in B:
+      return 0
+    if y >= Ym:
+      return -1
+    d = flow(x, y + 1)
+    if d == -1:
+      V[(x, y)] = -1
+      return -1
+    l = x - 1
+    while (l, y) not in B:
+      V[(l, y)] = -1
+      if flow(l, y + 1) != 0:
+        break
+      l -= 1
+    r = x + 1
+    while (r, y) not in B:
+      V[(r, y)] = -1
+      if flow(r, y + 1) != 0:
+        break
+      r += 1
+    if (l, y) in B and (r, y) in B:
+      for xx in range(l + 1, r):
+        V[(xx, y)] = 0
+      return 0
+    V[(x, y)] = -1
+    return -1
+
+  flow(500, 0)
+  print(len([y for x, y in V if V[(x, y)] == 0 and y in range(Ymm, Ym + 1)]))
 
 
 part1()
