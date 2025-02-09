4585202e7d1a5141cefcfc52890ee20760758956
AbdeI1 <abdelrahmanmmokbel@gmail.com>
2025-02-09 03:51:27 -0600

    2018 21, 22

--- 2018/21/code.py
+++ 2018/21/code.py
@@ -2,17 +2,146 @@ import os
 os.chdir(os.path.dirname(__file__))
 
 
+def addr(R, A, B, C):
+  R[C] = R[A] + R[B]
+
+
+def addi(R, A, B, C):
+  R[C] = R[A] + B
+
+
+def mulr(R, A, B, C):
+  R[C] = R[A] * R[B]
+
+
+def muli(R, A, B, C):
+  R[C] = R[A] * B
+
+
+def banr(R, A, B, C):
+  R[C] = R[A] & R[B]
+
+
+def bani(R, A, B, C):
+  R[C] = R[A] & B
+
+
+def borr(R, A, B, C):
+  R[C] = R[A] | R[B]
+
+
+def bori(R, A, B, C):
+  R[C] = R[A] | B
+
+
+def setr(R, A, B, C):
+  R[C] = R[A]
+
+
+def seti(R, A, B, C):
+  R[C] = A
+
+
+def gtir(R, A, B, C):
+  R[C] = 1 if A > R[B] else 0
+
+
+def gtri(R, A, B, C):
+  R[C] = 1 if R[A] > B else 0
+
+
+def gtrr(R, A, B, C):
+  R[C] = 1 if R[A] > R[B] else 0
+
+
+def eqir(R, A, B, C):
+  R[C] = 1 if A == R[B] else 0
+
+
+def eqri(R, A, B, C):
+  R[C] = 1 if R[A] == B else 0
+
+
+def eqrr(R, A, B, C):
+  R[C] = 1 if R[A] == R[B] else 0
+
+
 def reader():
   return open(f"input.txt", 'r').read().splitlines()
 
 
 def part1():
-  pass
+  f = [[l.split()[0]] + list(map(int, l.split()[1:])) for l in reader()]
+  ip = int(f[0][1])
+  f = f[1:]
+  R = [0 for _ in range(6)]
+  while R[ip] < len(f):
+    if R[ip] == 30:
+      break
+    op, A, B, C = f[R[ip]]
+    eval(op)(R, A, B, C)
+    R[ip] += 1
+  print(max(R))
 
 
 def part2():
-  pass
+  f = [[l.split()[0]] + list(map(int, l.split()[1:])) for l in reader()]
+  f = f[1:]
+
+  # while R[ip] < len(f):
+  #   op, A, B, C = f[R[ip]]
+  #   eval(op)(R, A, B, C)
+  #   R[ip] += 1
+
+  x1, x2 = f[7][1], max(f[11][1:3])
+
+  n = 0
+  L = []
+  G = set()
+  while n not in G:
+    G.add(n)
+    L.append(n)
+    a = n | (1 << 16)
+    n = x1
+    while (a > 0):
+      n = (((n + (a & 255)) & 16777215) * x2) & 16777215
+      a >>= 8
+
+  print(L[-1])
 
 
 part1()
 part2()
+
+# A B C D E ip
+# C = 123
+# C &= 456
+# C = (C == 72)
+# ip += C
+# ip = 0
+# C = 9
+# B = C | 65536
+# C = 1250634
+# E = B & 255
+# C += E
+# C &= 16777215
+# C *= 65899
+# C &= 16777215
+# E = (256 > B)
+# ip += E
+# ip += 1
+# ip = 27
+# E = 0
+# D = E + 1
+# D *= 256
+# D = (D > B)
+# ip += D
+# ip += 1
+# ip = 25
+# E += 1
+# ip = 17
+# B = E
+# ip = 7
+# E = (A == C)
+# ip += E
+# ip = 5
--- 2018/22/code.py
+++ 2018/22/code.py
@@ -1,5 +1,7 @@
 import os
 os.chdir(os.path.dirname(__file__))
+from functools import cache
+from heapq import heappop, heappush
 
 
 def reader():
@@ -7,11 +9,59 @@ def reader():
 
 
 def part1():
-  pass
+  f = reader()
+  depth = int(f[0].split(": ")[1])
+  tx, ty = eval(f[1].split(": ")[1])
+  M = 20183
+  E = [[0 for _ in range(tx + 1)] for _ in range(ty + 1)]
+  for y in range(ty + 1):
+    for x in range(tx + 1):
+      if y == 0:
+        E[y][x] = (x * 16807 + depth) % M
+      elif x == 0:
+        E[y][x] = (y * 48271 + depth) % M
+      else:
+        E[y][x] = (E[y - 1][x] * E[y][x - 1] + depth) % M
+  E[-1][-1] = depth % M
+  print(sum(i % 3 for r in E for i in r))
 
 
 def part2():
-  pass
+  f = reader()
+  depth = int(f[0].split(": ")[1])
+  tx, ty = eval(f[1].split(": ")[1])
+  M = 20183
+
+  @cache
+  def E(x, y):
+    if (x, y) == (tx, ty):
+      return depth % M
+    elif y == 0:
+      return (x * 16807 + depth) % M
+    elif x == 0:
+      return (y * 48271 + depth) % M
+    else:
+      return (E(x - 1, y) * E(x, y - 1) + depth) % M
+
+  Q = [(0, (0, 0, 1))]
+  V = set()
+  while Q:
+    d, (x, y, t) = heappop(Q)
+    if x < 0 or y < 0:
+      continue
+    if E(x, y) % 3 == t:
+      continue
+    if (x, y, t) in V:
+      continue
+    V.add((x, y, t))
+    # print(d, (x, y, t))
+    if (x, y, t) == (tx, ty, 1):
+      print(d)
+      break
+    heappush(Q, (d + 7, (x, y, (t + 1) % 3)))
+    heappush(Q, (d + 7, (x, y, (t + 2) % 3)))
+    for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+      heappush(Q, (d + 1, (x + dx, y + dy, t)))
 
 
 part1()
