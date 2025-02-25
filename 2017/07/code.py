32228d8d29b963c1ad602ab1c415144ac1031433
AbdeI1 <abdelrahmanmmokbel@gmail.com>
2025-02-25 16:21:24 -0600

    2017 7

--- 2017/07/code.py
+++ 2017/07/code.py
@@ -1,5 +1,7 @@
 import os
 os.chdir(os.path.dirname(__file__))
+from itertools import chain
+from collections import Counter
 
 
 def reader():
@@ -7,11 +9,52 @@ def reader():
 
 
 def part1():
-  pass
+  f = reader()
+  W, C = {}, {}
+  for l in f:
+    p = l.split(' -> ')
+    c = []
+    if len(p) > 1:
+      p, c = p[0], p[1].split(', ')
+    else:
+      p = p[0]
+    p, w = p.split(' (')
+    w = int(w[:-1])
+    W[p] = w
+    C[p] = c
+  print(next(iter(set(W) - set(chain(*C.values())))))
 
 
 def part2():
-  pass
+  f = reader()
+  W, C = {}, {}
+  for l in f:
+    p = l.split(' -> ')
+    c = []
+    if len(p) > 1:
+      p, c = p[0], p[1].split(', ')
+    else:
+      p = p[0]
+    p, w = p.split(' (')
+    w = int(w[:-1])
+    W[p] = w
+    C[p] = c
+  S = next(iter(set(W) - set(chain(*C.values()))))
+
+  def dfs(n):
+    if len(C[n]) == 0:
+      return W[n]
+    w = list(map(dfs, C[n]))
+    if len(set(w)) > 1:
+      (n1, _), (n2, _) = Counter(w).most_common()
+      for i in range(len(w)):
+        if w[i] == n2:
+          print(C[n][i])
+          print(W[C[n][i]] + (n1 - n2))
+          w[i] = n1
+    return W[n] + sum(w)
+
+  dfs(S)
 
 
 part1()
