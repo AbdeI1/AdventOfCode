5863c339df5b1529f5fbb25dcf16d7da8a13b35e
AbdeI1 <abdelrahmanmmokbel@gmail.com>
2025-02-28 16:41:53 -0600

    2017 9, 10

--- 2017/09/code.py
+++ 2017/09/code.py
@@ -1,5 +1,6 @@
 import os
 os.chdir(os.path.dirname(__file__))
+from re import sub
 
 
 def reader():
@@ -7,11 +8,47 @@ def reader():
 
 
 def part1():
-  pass
+  f = list(sub(r"!.", "", reader()[0]).replace(
+    "{", "[").replace("}", "]").replace('"', ""))
+  o = False
+  for i in range(len(f)):
+    if not o and f[i] == '<':
+      f[i] = '"'
+      o = True
+    elif o and f[i] == '>':
+      f[i] = '"'
+      o = False
+  f = ''.join(f)
+  s = eval(f)
+
+  def score(l, b=1):
+    if type(l) is str:
+      return 0
+    return b + sum(score(x, b + 1) for x in l)
+
+  print(score(s))
 
 
 def part2():
-  pass
+  f = list(sub(r"!.", "", reader()[0]).replace(
+    "{", "[").replace("}", "]").replace('"', "'"))
+  o = False
+  for i in range(len(f)):
+    if not o and f[i] == '<':
+      f[i] = '"'
+      o = True
+    elif o and f[i] == '>':
+      f[i] = '"'
+      o = False
+  f = ''.join(f)
+  s = eval(f)
+
+  def score(l):
+    if type(l) is str:
+      return len(l)
+    return sum(score(x) for x in l)
+
+  print(score(s))
 
 
 part1()
--- 2017/10/code.py
+++ 2017/10/code.py
@@ -1,5 +1,7 @@
 import os
 os.chdir(os.path.dirname(__file__))
+from functools import reduce
+from operator import xor
 
 
 def reader():
@@ -7,11 +9,33 @@ def reader():
 
 
 def part1():
-  pass
+  f = eval(reader()[0])
+  l = [i for i in range(256)]
+  i, s = 0, 0
+  for le in f:
+    for j in range(le // 2):
+      l[(i + j) % len(l)], l[(i + le - 1 - j) %
+                             len(l)] = l[(i + le - 1 - j) % len(l)], l[(i + j) % len(l)]
+    i += le + s
+    i %= len(l)
+    s += 1
+  print(l[0] * l[1])
 
 
 def part2():
-  pass
+  f = list(map(ord, reader()[0])) + [17, 31, 73, 47, 23]
+  l = [i for i in range(256)]
+  i, s = 0, 0
+  for _ in range(64):
+    for le in f:
+      for j in range(le // 2):
+        l[(i + j) % len(l)], l[(i + le - 1 - j) %
+                               len(l)] = l[(i + le - 1 - j) % len(l)], l[(i + j) % len(l)]
+      i += le + s
+      i %= len(l)
+      s += 1
+  print(
+    ''.join(f"{n:02x}" for n in [reduce(xor, l[16 * i:16 * i + 16]) for i in range(16)]))
 
 
 part1()
