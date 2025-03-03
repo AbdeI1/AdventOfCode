7c6678af2ee09f9fc0c529212faf95526d280f13
AbdeI1 <abdelrahmanmmokbel@gmail.com>
2025-03-03 16:38:10 -0600

    2017 11

--- 2017/11/code.py
+++ 2017/11/code.py
@@ -7,11 +7,40 @@ def reader():
 
 
 def part1():
-  pass
+  f = reader()[0].split(',')
+  x, y = 0, 0
+  M = {
+    'n': (0, 2),
+    's': (0, -2),
+    'ne': (1, 1),
+    'nw': (-1, 1),
+    'se': (1, -1),
+    'sw': (-1, -1)
+  }
+  for c in f:
+    dx, dy = M[c]
+    x, y = x + dx, y + dy
+  print(abs(abs(y) - abs(x)) // 2 + min(abs(x), abs(y)))
 
 
 def part2():
-  pass
+  f = reader()[0].split(',')
+  x, y = 0, 0
+  M = {
+    'n': (0, 2),
+    's': (0, -2),
+    'ne': (1, 1),
+    'nw': (-1, 1),
+    'se': (1, -1),
+    'sw': (-1, -1)
+  }
+  md = 0
+  for c in f:
+    dx, dy = M[c]
+    x, y = x + dx, y + dy
+    d = abs(abs(y) - abs(x)) // 2 + min(abs(x), abs(y))
+    md = max(d, md)
+  print(md)
 
 
 part1()
