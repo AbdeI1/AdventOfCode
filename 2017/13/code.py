68e423929d461b391e8f76c0e553e09e3da9ce40
AbdeI1 <abdelrahmanmmokbel@gmail.com>
2025-03-03 17:20:55 -0600

    2017 13

--- 2017/13/code.py
+++ 2017/13/code.py
@@ -7,11 +7,17 @@ def reader():
 
 
 def part1():
-  pass
+  print(sum(t * d for t, d in eval(f'{{{', '.join(reader())}}}').items() if t % (2 * (d - 1)) == 0))
 
 
 def part2():
-  pass
+  f = eval(f'{{{', '.join(reader())}}}')
+  M = [(2 * (d - 1)) for _, d in f.items()]
+  l = [t % (2 * (d - 1)) for t, d in f.items()]
+  D = 0
+  while 0 in [(t + D) % (2 * (d - 1)) for t, d in f.items()]:
+    D += 2
+  print(D)
 
 
 part1()
