6c11ed759319e2b93b2dd4905bd38aff9d4c4f57
AbdeI1 <abdelrahmanmmokbel@gmail.com>
2025-03-05 15:55:01 -0600

    2017 15

--- 2017/15/code.py
+++ 2017/15/code.py
@@ -7,12 +7,34 @@ def reader():
 
 
 def part1():
-  pass
+  A, B = map(lambda s: int(s.split()[-1]), reader())
+
+  s = 0
+  for _ in range(40000000):
+    A = (A * 16807) % 2147483647
+    B = (B * 48271) % 2147483647
+    if (A & 0xffff) == (B & 0xffff):
+      s += 1
+
+  print(s)
 
 
 def part2():
-  pass
+  A, B = map(lambda s: int(s.split()[-1]), reader())
+
+  s = 0
+  for _ in range(5000000):
+    A = (A * 16807) % 2147483647
+    while A % 4 != 0:
+      A = (A * 16807) % 2147483647
+    B = (B * 48271) % 2147483647
+    while B % 8 != 0:
+      B = (B * 48271) % 2147483647
+    if (A & 0xffff) == (B & 0xffff):
+      s += 1
+
+  print(s)
 
 
-part1()
+# part1()
 part2()
