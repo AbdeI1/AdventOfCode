adbcf0f551b581509d1fc9545d4fd10501d3be89
AbdeI1 <abdelrahmanmmokbel@gmail.com>
2025-03-12 12:07:47 -0500

    rotation and dictionary built

--- 2017/21/code.py
+++ 2017/21/code.py
@@ -7,7 +7,41 @@ def reader():
 
 
 def part1():
-  pass
+  f = reader()
+  R = [
+    {},
+    {
+      (0, 0): (0, 0)
+    },
+    {
+      (0, 0): (0, 1),
+      (0, 1): (1, 1),
+      (1, 1): (1, 0),
+      (1, 0): (0, 0)
+    },
+    {
+      (0, 0): (0, 2),
+      (0, 1): (1, 2),
+      (0, 2): (2, 2),
+      (1, 0): (0, 1),
+      (1, 1): (1, 1),
+      (1, 2): (2, 1),
+      (2, 0): (0, 0),
+      (2, 1): (1, 0),
+      (2, 2): (2, 0)
+    }
+  ]
+  T = {}
+  for l in f:
+    a, b = l.split(' => ')
+    a = a.split('/')
+    b = b.split('/')
+    for _ in range(2):
+      for _ in range(4):
+        T[tuple(a)] = b
+        a = [''.join([a[R[len(a)][(i, j)][0]][R[len(a)][(i, j)][1]] for j in range(len(a))]) for i in range(len(a))]
+      a = a[::-1]
+  print(len(T.keys()))
 
 
 def part2():
