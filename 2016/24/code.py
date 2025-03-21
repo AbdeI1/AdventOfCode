7ec9d5c72684a109b0fc7c8c94c44e6034b2ac8c
AbdeI1 <abdelrahmanmmokbel@gmail.com>
2025-03-21 13:34:19 -0500

    2016 24

--- 2016/24/code.py
+++ 2016/24/code.py
@@ -1,5 +1,6 @@
 import os
 os.chdir(os.path.dirname(__file__))
+from itertools import permutations, pairwise
 
 
 def reader():
@@ -7,11 +8,51 @@ def reader():
 
 
 def part1():
-  pass
+  M = reader()
+  N = {int(n): {} for r in M for n in r if n.isdigit()}
+  for n, D in N.items():
+    (i, j) = next((i, j) for i, r in enumerate(M)
+                  for j, c in enumerate(r) if c == f'{n}')
+    Q = [(i, j, 0)]
+    V = set()
+    while Q:
+      ii, jj, d = Q.pop(0)
+      if (ii, jj) in V:
+        continue
+      V.add((ii, jj))
+      if M[ii][jj] == '#':
+        continue
+      if M[ii][jj].isdigit():
+        D[int(M[ii][jj])] = d
+      for di, dj in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+        Q.append((ii + di, jj + dj, d + 1))
+  G = [[N[i][j] for j in range(len(N))] for i in range(len(N))]
+  print(min(sum(G[x][y] for x, y in pairwise((0, ) + p))
+        for p in permutations(range(1, len(N)), len(N) - 1)))
 
 
 def part2():
-  pass
+  M = reader()
+  N = {int(n): {} for r in M for n in r if n.isdigit()}
+  for n, D in N.items():
+    (i, j) = next((i, j) for i, r in enumerate(M)
+                  for j, c in enumerate(r) if c == f'{n}')
+    Q = [(i, j, 0)]
+    V = set()
+    while Q:
+      ii, jj, d = Q.pop(0)
+      if (ii, jj) in V:
+        continue
+      V.add((ii, jj))
+      if M[ii][jj] == '#':
+        continue
+      if M[ii][jj].isdigit():
+        D[int(M[ii][jj])] = d
+      for di, dj in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+        Q.append((ii + di, jj + dj, d + 1))
+  G = [[N[i][j] for j in range(len(N))] for i in range(len(N))]
+  print(min(sum(G[x][y] for x, y in pairwise((0, ) + p + (0, )))
+        for p in permutations(range(1, len(N)), len(N) - 1)))
 
 
 part1()
