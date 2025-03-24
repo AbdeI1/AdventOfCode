8105a8a35e845c6b852cc8c6ea0fab361876e557
AbdeI1 <abdelrahmanmmokbel@gmail.com>
2025-03-24 16:22:14 -0500

    2015 24

--- 2015/24/code.py
+++ 2015/24/code.py
@@ -1,5 +1,7 @@
 import os
 os.chdir(os.path.dirname(__file__))
+from functools import cache
+from math import prod
 
 
 def reader():
@@ -7,11 +9,43 @@ def reader():
 
 
 def part1():
-  pass
+  W = list(map(int, reader()))
+  T = sum(W) // 3
+
+  @cache
+  def f(t=T, i=0):
+    if i >= len(W):
+      return [[]] if t == 0 else []
+    if t == 0:
+      return [[]]
+    if t < 0:
+      return []
+    return f(t, i + 1) + [[W[i]] + l for l in f(t - W[i], i + 1)]
+
+  L = f()
+  l = len(min(L, key=lambda l: len(l)))
+  print(
+    prod(sorted(filter(lambda x: len(x) == l, L), key=lambda x: prod(x))[0]))
 
 
 def part2():
-  pass
+  W = list(map(int, reader()))
+  T = sum(W) // 4
+
+  @cache
+  def f(t=T, i=0):
+    if i >= len(W):
+      return [[]] if t == 0 else []
+    if t == 0:
+      return [[]]
+    if t < 0:
+      return []
+    return f(t, i + 1) + [[W[i]] + l for l in f(t - W[i], i + 1)]
+
+  L = f()
+  l = len(min(L, key=lambda l: len(l)))
+  print(
+    prod(sorted(filter(lambda x: len(x) == l, L), key=lambda x: prod(x))[0]))
 
 
 part1()
