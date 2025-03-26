31341d74fc894ba80d796f5535acc745cc03fa88
AbdeI1 <abdelrahmanmmokbel@gmail.com>
2025-03-25 22:23:39 -0500

    2015 23, 2016 22 p1

--- 2015/23/code.py
+++ 2015/23/code.py
@@ -1,5 +1,6 @@
 import os
 os.chdir(os.path.dirname(__file__))
+from collections import defaultdict
 
 
 def reader():
@@ -7,11 +8,60 @@ def reader():
 
 
 def part1():
-  pass
+  p = list(map(lambda s: s.replace(",", "").split(), reader()))
+  R = defaultdict(int)
+  i = 0
+  while i in range(len(p)):
+    l = p[i]
+    match l[0]:
+      case 'hlf':
+        R[l[1]] //= 2
+      case 'tpl':
+        R[l[1]] *= 3
+      case 'inc':
+        R[l[1]] += 1
+      case 'jmp':
+        i += int(l[1])
+        continue
+      case 'jie':
+        if R[l[1]] % 2 == 0:
+          i += int(l[2])
+          continue
+      case 'jio':
+        if R[l[1]] == 1:
+          i += int(l[2])
+          continue
+    i += 1
+  print(R['b'])
 
 
 def part2():
-  pass
+  p = list(map(lambda s: s.replace(",", "").split(), reader()))
+  R = defaultdict(int)
+  R['a'] = 1
+  i = 0
+  while i in range(len(p)):
+    l = p[i]
+    match l[0]:
+      case 'hlf':
+        R[l[1]] //= 2
+      case 'tpl':
+        R[l[1]] *= 3
+      case 'inc':
+        R[l[1]] += 1
+      case 'jmp':
+        i += int(l[1])
+        continue
+      case 'jie':
+        if R[l[1]] % 2 == 0:
+          i += int(l[2])
+          continue
+      case 'jio':
+        if R[l[1]] == 1:
+          i += int(l[2])
+          continue
+    i += 1
+  print(R['b'])
 
 
 part1()
--- 2016/22/code.py
+++ 2016/22/code.py
@@ -1,5 +1,6 @@
 import os
 os.chdir(os.path.dirname(__file__))
+from itertools import permutations
 
 
 def reader():
@@ -7,11 +8,26 @@ def reader():
 
 
 def part1():
-  pass
+  f = reader()
+  l = f[-1].split('-')
+  X = int(l[1][1:]) + 1
+  Y = int(l[2].split()[0][1:]) + 1
+  G = [[tuple(map(lambda s: int(s[:-1]), f[2 + x * Y + y].split()[2:4]))
+        for x in range(X)] for y in range(Y)]
+  print(len(list(((x1, y1), (x2, y2)) for (x1, y1), (x2, y2)
+        in permutations(((x, y) for x in range(X) for y in range(Y)), 2)
+        if G[y1][x1][0] > 0 and G[y1][x1][0] <= G[y2][x2][1])))
 
 
 def part2():
-  pass
+  f = reader()
+  l = f[-1].split('-')
+  X = int(l[1][1:]) + 1
+  Y = int(l[2].split()[0][1:]) + 1
+  G = [[tuple(map(lambda s: int(s[:-1]), f[2 + x * Y + y].split()[2:4]))
+        for x in range(X)] for y in range(Y)]
+  x, y = next((x, y) for x in range(X) for y in range(Y) if G[y][x][0] == 0)
+  print(x, y, X, Y)
 
 
 part1()
