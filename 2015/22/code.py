1c1878888f602326a3151dfb5e136edfc95723ea
AbdeI1 <abdelrahmanmmokbel@gmail.com>
2025-03-28 18:29:18 -0500

    2015 22, 2016 21

--- 2015/22/code.py
+++ 2015/22/code.py
@@ -1,5 +1,6 @@
 import os
 os.chdir(os.path.dirname(__file__))
+from heapq import heappush, heappop
 
 
 def reader():
@@ -7,11 +8,107 @@ def reader():
 
 
 def part1():
-  pass
+  f = reader()
+  hp = int(f[0].split()[-1])
+  dmg = int(f[1].split()[-1])
+  Q = [(0, 50, 0, 500, hp, 0, 0, 0, 0)]
+  while Q:
+    mana_u, php, parm, mana, bhp, shield, poison, recharge, t = heappop(Q)
+
+    if shield > 0:
+      shield -= 1
+      if shield == 0:
+        parm = 0
+
+    if poison > 0:
+      bhp -= 3
+      poison -= 1
+
+    if recharge > 0:
+      mana += 101
+      recharge -= 1
+
+    if bhp <= 0:
+      print(mana_u)
+      break
+
+    if php <= 0:
+      continue
+
+    if t == 0:
+      if mana >= 53:
+        heappush(Q, (mana_u + 53, php, parm, mana - 53,
+                     bhp - 4, shield, poison, recharge, 1))
+      if mana >= 73:
+        heappush(Q, (mana_u + 73, php + 2, parm, mana -
+                     73, bhp - 2, shield, poison, recharge, 1))
+      if mana >= 113 and shield == 0:
+        heappush(Q, (mana_u + 113, php, 7, mana -
+                 113, bhp, 6, poison, recharge, 1))
+      if mana >= 173 and poison == 0:
+        heappush(Q, (mana_u + 173, php, parm, mana -
+                 173, bhp, shield, 6, recharge, 1))
+      if mana >= 229 and recharge == 0:
+        heappush(Q, (mana_u + 229, php, parm, mana -
+                 229, bhp, shield, poison, 5, 1))
+    else:
+      heappush(Q, (mana_u, php - max(1, dmg - parm), parm,
+               mana, bhp, shield, poison, recharge, 0))
 
 
 def part2():
-  pass
+  f = reader()
+  hp = int(f[0].split()[-1])
+  dmg = int(f[1].split()[-1])
+  Q = [(0, 50, 0, 500, hp, 0, 0, 0, 0)]
+  while Q:
+    mana_u, php, parm, mana, bhp, shield, poison, recharge, t = heappop(Q)
+
+    if t == 0:
+      php -= 1
+
+    if php <= 0:
+      continue
+
+    if shield > 0:
+      shield -= 1
+      if shield == 0:
+        parm = 0
+
+    if poison > 0:
+      bhp -= 3
+      poison -= 1
+
+    if recharge > 0:
+      mana += 101
+      recharge -= 1
+
+    if bhp <= 0:
+      print(mana_u)
+      break
+
+    if php <= 0:
+      continue
+
+    if t == 0:
+      if mana >= 53:
+        heappush(Q, (mana_u + 53, php, parm, mana - 53,
+                     bhp - 4, shield, poison, recharge, 1))
+      if mana >= 73:
+        heappush(Q, (mana_u + 73, php + 2, parm, mana -
+                     73, bhp - 2, shield, poison, recharge, 1))
+      if mana >= 113 and shield == 0:
+        heappush(Q, (mana_u + 113, php, 7, mana -
+                 113, bhp, 6, poison, recharge, 1))
+      if mana >= 173 and poison == 0:
+        heappush(Q, (mana_u + 173, php, parm, mana -
+                 173, bhp, shield, 6, recharge, 1))
+      if mana >= 229 and recharge == 0:
+        heappush(Q, (mana_u + 229, php, parm, mana -
+                 229, bhp, shield, poison, 5, 1))
+    else:
+      heappush(Q, (mana_u, php - max(1, dmg - parm), parm,
+               mana, bhp, shield, poison, recharge, 0))
 
 
 part1()
--- 2016/21/code.py
+++ 2016/21/code.py
@@ -7,11 +7,68 @@ def reader():
 
 
 def part1():
-  pass
+  f = [l.split() for l in reader()]
+  s = list("abcdefgh")
+  for l in f:
+    if l[0] == "swap" and l[1] == "position":
+      x, y = int(l[2]), int(l[5])
+      s[x], s[y] = s[y], s[x]
+    elif l[0] == "swap" and l[1] == "letter":
+      x, y = l[2], l[5]
+      i, j = s.index(x), s.index(y)
+      s[i], s[j] = y, x
+    elif l[0] == "rotate" and l[1] == "left":
+      x = int(l[2]) % len(s)
+      s = s[x:] + s[:x]
+    elif l[0] == "rotate" and l[1] == "right":
+      x = int(l[2]) % len(s)
+      s = s[-x:] + s[:-x]
+    elif l[0] == "rotate" and l[1] == "based":
+      x = l[6]
+      i = s.index(x)
+      r = (1 + i + (1 if i >= 4 else 0)) % len(s)
+      s = s[-r:] + s[:-r]
+    elif l[0] == "reverse":
+      x, y = int(l[2]), int(l[4])
+      s = s[:x] + s[x:y + 1][::-1] + s[y + 1:]
+    elif l[0] == "move":
+      x, y = int(l[2]), int(l[5])
+      ll = s.pop(x)
+      s.insert(y, ll)
+  print(''.join(s))
 
 
 def part2():
-  pass
+  f = [l.split() for l in reader()]
+  s = list("fbgdceah")
+  for l in f[::-1]:
+    if l[0] == "swap" and l[1] == "position":
+      x, y = int(l[2]), int(l[5])
+      s[x], s[y] = s[y], s[x]
+    elif l[0] == "swap" and l[1] == "letter":
+      x, y = l[2], l[5]
+      i, j = s.index(x), s.index(y)
+      s[i], s[j] = y, x
+    elif l[0] == "rotate" and l[1] == "left":
+      x = int(l[2]) % len(s)
+      s = s[-x:] + s[:-x]
+    elif l[0] == "rotate" and l[1] == "right":
+      x = int(l[2]) % len(s)
+      s = s[x:] + s[:x]
+    elif l[0] == "rotate" and l[1] == "based":
+      x = l[6]
+      i = (s.index(x) - 1) % len(s)
+      i = i // 2 + (4 if i % 2 == 1 else 0)
+      r = (1 + i + (1 if i >= 4 else 0)) % len(s)
+      s = s[r:] + s[:r]
+    elif l[0] == "reverse":
+      x, y = int(l[2]), int(l[4])
+      s = s[:x] + s[x:y + 1][::-1] + s[y + 1:]
+    elif l[0] == "move":
+      x, y = int(l[2]), int(l[5])
+      ll = s.pop(y)
+      s.insert(x, ll)
+  print(''.join(s))
 
 
 part1()
