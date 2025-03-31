b3b99691ad7930ac84059fe287d3df7daf15455f
AbdeI1 <abdelrahmanmmokbel@gmail.com>
2025-03-31 15:55:47 -0500

    more problems

--- 2015/20/code.py
+++ 2015/20/code.py
@@ -1,5 +1,6 @@
 import os
 os.chdir(os.path.dirname(__file__))
+from math import prod
 
 
 def reader():
@@ -7,11 +8,27 @@ def reader():
 
 
 def part1():
-  pass
+  n = int(reader()[0])
+  F = [0 for _ in range(1000000)]
+  for i in range(1, len(F)):
+    F[i] += i * 10
+    if F[i] > n:
+      print(i)
+      break
+    for j in range(i + i, len(F), i):
+      F[j] += i * 10
 
 
 def part2():
-  pass
+  n = int(reader()[0])
+  F = [0 for _ in range(1000000)]
+  for i in range(1, len(F)):
+    F[i] += i * 11
+    if F[i] > n:
+      print(i)
+      break
+    for j in range(i + i, min(len(F), 50 * i + 1), i):
+      F[j] += i * 11
 
 
 part1()
--- 2016/20/code.py
+++ 2016/20/code.py
@@ -1,5 +1,6 @@
 import os
 os.chdir(os.path.dirname(__file__))
+from itertools import pairwise
 
 
 def reader():
@@ -7,11 +8,32 @@ def reader():
 
 
 def part1():
-  pass
+  f = sorted([eval(l.replace('-', ',')) for l in reader()])
+  if f[0][0] > 0:
+    print(0)
+    return
+  for (_, e), (b, _) in pairwise(f):
+    if b > e + 1:
+      print(e + 1)
+      break
 
 
 def part2():
-  pass
+  f = sorted([eval(l.replace('-', ',')) for l in reader()])
+  I = []
+  s, e = f[0]
+  for i, j in f[1:]:
+    if i > e + 1:
+      I.append((s, e))
+      s, e = i, j
+    e = max(e, j)
+  I.append((s, e))
+  A = I[0][0]
+  for (_, i), (j, _) in pairwise(I):
+    A += j - i - 1
+  print(I)
+  A += (1 << 32) - I[-1][-1] - 1
+  print(A)
 
 
 part1()
