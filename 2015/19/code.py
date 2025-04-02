f73518bcf6864ef71b11b5a8784dd5f3b10a39cc
AbdeI1 <abdelrahmanmmokbel@gmail.com>
2025-04-02 16:06:01 -0500

    2015 19

--- 2015/19/code.py
+++ 2015/19/code.py
@@ -1,5 +1,7 @@
 import os
 os.chdir(os.path.dirname(__file__))
+from collections import defaultdict
+from re import finditer, search
 
 
 def reader():
@@ -7,11 +9,31 @@ def reader():
 
 
 def part1():
-  pass
+  f = reader()
+  S = set()
+  M = f[-1]
+  for l in f[:-2]:
+    b, e = l.split(' => ')
+    for m in finditer(b, M):
+      S.add(M[:m.start()] + e + M[m.end():])
+  print(len(S))
 
 
 def part2():
-  pass
+  f = reader()
+  R = {}
+  M = f[-1]
+  for l in f[:-2]:
+    b, e = l.split(' => ')
+    R[e] = b
+  A = 0
+  while M != 'e':
+    for r, rr in sorted(R.items(), key=lambda t: -len(t[0])):
+      if m := search(r, M):
+        M = M[:m.start()] + rr + M[m.end():]
+        A += 1
+        break
+  print(A)
 
 
 part1()
--- wrapper.py
+++ wrapper.py
@@ -60,7 +60,7 @@ def submit(year, day):
     aocd_submit(out[1], part="b", year=year, day=day, session=token)
 
 
-y, d = 2015, 20
+y, d = 2015, 19
 
 fetch(y, d)
 submit(y, d)
