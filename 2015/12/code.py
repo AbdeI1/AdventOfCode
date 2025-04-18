1633638a0d13d93ad4cb09d57cb82f868bb80415
AbdeI1 <abdelrahmanmmokbel@gmail.com>
2025-04-18 18:52:22 -0500

    2016 2015 12

--- 2015/12/code.py
+++ 2015/12/code.py
@@ -7,11 +7,40 @@ def reader():
 
 
 def part1():
-  pass
+  D = eval(reader()[0])
+
+  def ssum(d):
+    if type(d) is str:
+      return 0
+    if type(d) is int:
+      return d
+    if type(d) is list:
+      return sum([ssum(i) for i in d])
+    if type(d) is dict:
+      return sum([ssum(v) for k, v in d.items()])
+
+  print(ssum(D))
 
 
 def part2():
-  pass
+  D = eval(reader()[0])
+
+  def ssum(d):
+    if type(d) is str:
+      return 0
+    if type(d) is int:
+      return d
+    if type(d) is list:
+      return sum([ssum(i) for i in d])
+    if type(d) is dict:
+      x = 0
+      for _, v in d.items():
+        if v == 'red':
+          return 0
+        x += ssum(v)
+      return x
+
+  print(ssum(D))
 
 
 part1()
--- 2016/12/code.py
+++ 2016/12/code.py
@@ -1,5 +1,7 @@
 import os
 os.chdir(os.path.dirname(__file__))
+from collections import defaultdict
+from functools import cache
 
 
 def reader():
@@ -7,11 +9,35 @@ def reader():
 
 
 def part1():
-  pass
+  f = [l.split() for l in reader()]
+  R = defaultdict(int)
+  def val(x): return R[x] if x.isalpha() else int(x)
+  i = 0
+  while i in range(len(f)):
+    l = f[i]
+    match l[0]:
+      case 'cpy':
+        R[l[2]] = val(l[1])
+      case 'inc':
+        R[l[1]] += 1
+      case 'dec':
+        R[l[1]] -= 1
+      case 'jnz':
+        if val(l[1]) != 0:
+          i += val(l[2])
+          continue
+    i += 1
+  print(R['a'])
 
 
 def part2():
-  pass
+  f = [l.split() for l in reader()]
+  R = defaultdict(int)
+
+  def fib(n):
+    return 1 if n <= 1 else fib(n - 1) + fib(n - 2)
+
+  print(fib(eval(f'{f[2][1]}+{f[5][1]}+1')) + eval(f'{f[16][1]}*{f[17][1]}'))
 
 
 part1()
