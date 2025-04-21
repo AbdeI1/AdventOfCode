7273f20d98a5066698616d7a612ad273021f9f60
AbdeI1 <abdelrahmanmmokbel@gmail.com>
2025-04-21 01:53:41 -0500

    more problems

--- 2015/11/code.py
+++ 2015/11/code.py
@@ -5,13 +5,36 @@ os.chdir(os.path.dirname(__file__))
 def reader():
   return open(f"input.txt", 'r').read().splitlines()
 
+# just do both parts of this problem in your head, below code might not work for all
+
 
 def part1():
-  pass
+  s = list(map(ord, reader()[0]))
+  if s[4] <= s[3] <= ord('x') and s[3] not in range(ord('i') - 2, ord('p')) and s[3:] != [s[3], s[3], s[3] + 1, s[3] + 2, s[3] + 2]:
+    s[3:] = [s[3], s[3], s[3] + 1, s[3] + 2, s[3] + 2]
+  elif s[4] <= s[3] <= ord('x') and s[3:] != [s[3], s[3], s[3] + 1, s[3] + 2, s[3] + 2]:
+    s[3:] = [ord('p'), ord('p'), ord('q'), ord('r'), ord('r')]
+  else:
+    s[2:] = [s[2] + (2 if s[2] in {ord('h'), ord('k'), ord('n')}
+                     else 1), ord('a'), ord('a'), ord('b'), ord('c'), ord('c')]
+  print(''.join(map(chr, s)))
 
 
 def part2():
-  pass
+  s = list(map(ord, reader()[0]))
+
+  def f(s):
+    if s[4] <= s[3] <= ord('x') and s[3] not in range(ord('i') - 2, ord('p')) and s[3:] != [s[3], s[3], s[3] + 1, s[3] + 2, s[3] + 2]:
+      s[3:] = [s[3], s[3], s[3] + 1, s[3] + 2, s[3] + 2]
+    elif s[4] <= s[3] <= ord('x') and s[3:] != [s[3], s[3], s[3] + 1, s[3] + 2, s[3] + 2]:
+      s[3:] = [ord('p'), ord('p'), ord('q'), ord('r'), ord('r')]
+    else:
+      s[2:] = [s[2] + (2 if s[2] in {ord('h'), ord('k'), ord('n')}
+                       else 1), ord('a'), ord('a'), ord('b'), ord('c'), ord('c')]
+
+  f(s)
+  f(s)
+  print(''.join(map(chr, s)))
 
 
 part1()
--- 2016/10/code.py
+++ 2016/10/code.py
@@ -1,5 +1,6 @@
 import os
 os.chdir(os.path.dirname(__file__))
+from collections import defaultdict
 
 
 def reader():
@@ -7,11 +8,75 @@ def reader():
 
 
 def part1():
-  pass
+  f = [l.split() for l in reader()]
+  I = {}
+  B = defaultdict(list)
+  O = defaultdict(list)
+  for l in f:
+    if l[0] == 'bot':
+      I[int(l[1])] = ((l[5], int(l[6])), (l[10], int(l[11])))
+    else:
+      B[int(l[5])].append(int(l[1]))
+
+  def r(i):
+    if len(B[i]) == 2:
+      v1, v2 = sorted(B[i])
+      if (v1, v2) == (17, 61):
+        print(i)
+      (I1d, I1n), (I2d, I2n) = I[i]
+      if I1d == 'bot':
+        B[I1n].append(v1)
+        r(I1n)
+      else:
+        O[I1n].append(v1)
+      if I2d == 'bot':
+        B[I2n].append(v2)
+        r(I2n)
+      else:
+        O[I2n].append(v2)
+      B[i] = []
+
+  s = -1
+  for n in B:
+    if len(B[n]) == 2:
+      s = n
+  r(s)
 
 
 def part2():
-  pass
+  f = [l.split() for l in reader()]
+  I = {}
+  B = defaultdict(list)
+  O = defaultdict(list)
+  for l in f:
+    if l[0] == 'bot':
+      I[int(l[1])] = ((l[5], int(l[6])), (l[10], int(l[11])))
+    else:
+      B[int(l[5])].append(int(l[1]))
+
+  def r(i):
+    if len(B[i]) == 2:
+      v1, v2 = sorted(B[i])
+      (I1d, I1n), (I2d, I2n) = I[i]
+      if I1d == 'bot':
+        B[I1n].append(v1)
+        r(I1n)
+      else:
+        O[I1n].append(v1)
+      if I2d == 'bot':
+        B[I2n].append(v2)
+        r(I2n)
+      else:
+        O[I2n].append(v2)
+      B[i] = []
+
+  s = -1
+  for n in B:
+    if len(B[n]) == 2:
+      s = n
+  r(s)
+
+  print(O[0][0] * O[1][0] * O[2][0])
 
 
 part1()
