5faf2fa51f3529712aee6adbb77f99938e12a381
AbdeI1 <abderahmanmmokbel@gmail.com>
2025-07-19 22:57:25 -0500

    updated cs to match scriptcs

--- 2022/01/code.cs
+++ 2022/01/code.cs
@@ -1,52 +1,50 @@
 using System;
 using System.Collections.Generic;
 
-class code
+static string[] reader()
 {
-  static string[] reader()
-  {
-    string[] f = System.IO.File.ReadAllLines("./input.txt");
-    return f;
-  }
-  static void part1()
-  {
-    string[] f = reader();
-    int ans = -1;
-    int s = 0;
-    foreach (string l in f)
-    {
-      if (l == "")
-      {
-        ans = Math.Max(ans, s);
-        s = 0;
-        continue;
-      }
-      s += Int32.Parse(l);
-    }
-    Console.WriteLine(ans);
-  }
-  static void part2()
-  {
-    string[] f = reader();
-    List<int> a = new List<int>();
-    int s = 0;
-    foreach (string l in f)
-    {
-      if (l == "")
-      {
-        a.Add(s);
-        s = 0;
-        continue;
-      }
-      s += Int32.Parse(l);
-    }
-    a.Sort();
-    a.Reverse();
-    Console.WriteLine(a[0] + a[1] + a[2]);
-  }
-  static void Main(string[] args)
-  {
-    part1();
-    part2();
-  }
+  string[] f = System.IO.File.ReadAllLines("./input.txt");
+  return f;
 }
+
+static void part1()
+{
+  string[] f = reader();
+  int ans = -1;
+  int s = 0;
+  foreach (string l in f)
+  {
+    if (l == "")
+    {
+      ans = Math.Max(ans, s);
+      s = 0;
+      continue;
+    }
+    s += Int32.Parse(l);
+  }
+  Console.WriteLine(ans);
+}
+
+static void part2()
+{
+  string[] f = reader();
+  List<int> a = new List<int>();
+  int s = 0;
+  foreach (string l in f)
+  {
+    if (l == "")
+    {
+      a.Add(s);
+      s = 0;
+      continue;
+    }
+    s += Int32.Parse(l);
+  }
+  a.Sort();
+  a.Reverse();
+  Console.WriteLine(a[0] + a[1] + a[2]);
+}
+
+part1();
+part2();
+
