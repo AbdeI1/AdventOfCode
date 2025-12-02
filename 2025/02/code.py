d4c23456086fb592cf20bed1b7e65dc34a094f33
AbdeI1 <abderahmanmmokbel@gmail.com>
2025-12-01 23:11:25 -0600

    2025 day 2

--- 2025/02/code.py
+++ 2025/02/code.py
@@ -7,12 +7,29 @@ def reader():
 
 
 def part1():
-  pass
+  f = reader()
+  R = [tuple(map(int, r.split('-'))) for r in f[0].split(',')]
+  A = 0
+  for r in R:
+    for n in range(r[0], r[1] + 1):
+      s = str(n)
+      if len(s) % 2 == 0 and s[:len(s)//2] == s[len(s)//2:]:
+        A += n
+  print(A)
 
 
 def part2():
-  pass
-
+  f = reader()
+  R = [tuple(map(int, r.split('-'))) for r in f[0].split(',')]
+  A = 0
+  for r in R:
+    for n in range(r[0], r[1] + 1):
+      s = str(n)
+      for l in range(1, len(s) // 2 + 1):
+        if len(s) % l == 0 and s == s[:l] * (len(s) // l):
+          A += n
+          break
+  print(A)
 
 part1()
 part2()
--- wrapper.py
+++ wrapper.py
@@ -60,7 +60,7 @@ def submit(year, day):
     aocd_submit(out[1], part="b", year=year, day=day, session=token)
 
 
-y, d = 2025, 1
+y, d = 2025, 2
 
 fetch(y, d)
 submit(y, d)
