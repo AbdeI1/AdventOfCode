eede63e869898d89787f0c37f44da491cb79efdb
AbdeI1 <abdelrahmanmmokbel@gmail.com>
2025-12-06 23:09:45 -0600

    2025 day 7, ~174

--- 2025/07/code.py
+++ 2025/07/code.py
@@ -1,17 +1,37 @@
 import os
 os.chdir(os.path.dirname(__file__))
-
+from collections import defaultdict
 
 def reader():
   return open(f"input.txt", 'r').read().splitlines()
 
 
 def part1():
-  pass
+  f = reader()
+  S = {f[0].index('S')}
+  A = 0
+  for l in f:
+    for j in range(len(l)):
+      if l[j] == '^' and j in S:
+        S.remove(j)
+        S.add(j - 1)
+        S.add(j + 1)
+        A += 1
+  print(A)
 
 
 def part2():
-  pass
+  f = reader()
+  S = defaultdict(int, {f[0].index('S'): 1})
+  A = 1
+  for l in f:
+    for j in range(len(l)):
+      if l[j] == '^' and j in S:
+        S[j - 1] += S[j]
+        S[j + 1] += S[j]
+        A += S[j]
+        S[j] = 0
+  print(A)
 
 
 part1()
--- wrapper.py
+++ wrapper.py
@@ -60,7 +60,7 @@ def submit(year, day):
     aocd_submit(out[1], part="b", year=year, day=day, session=token)
 
 
-y, d = 2025, 6
+y, d = 2025, 7
 
 fetch(y, d)
 submit(y, d)
