63d41198ed984d148e7da9062a1e508aa325cfab
AbdeI1 <abdelrahmanmmokbel@gmail.com>
2025-12-10 12:16:59 -0600

    sovled with linear equation solver

--- 2025/10/code.py
+++ 2025/10/code.py
@@ -2,6 +2,8 @@ import os
 os.chdir(os.path.dirname(__file__))
 from collections import deque
 from heapq import heappop, heappush
+import numpy
+from pulp import *
 
 
 def reader():
@@ -34,7 +36,18 @@ def part2():
   f = [(l[1:l.find(']')], list(map(lambda x: eval(f'{{{x[1:-1]}}}'), l[l.find('('):l.find('{')-1].split())), eval(f'({l[l.find('{')+1:-1]})')) for l in reader()]
   A = 0
   for _, B, E in f:
-    print([min(E[i] for i in b) for b in B])
+    M = numpy.array([[1 if i in b else 0 for i in range(len(E))] for b in B]).T
+    Y = numpy.array(E)
+    # X = M.T @ numpy.linalg.inv(M @ M.T) @ Y
+    X = [LpVariable(f"x{i}", 0, cat=LpInteger) for i in range(len(B))]
+    prob = LpProblem("problem", LpMinimize)
+    for i in range(len(M)):
+      prob += sum(X[j] if M[i][j] == 1 else 0 for j in range(len(M[i]))) == Y[i]
+    prob += sum(X)
+    prob.solve(PULP_CBC_CMD(msg=0))
+    s = int(sum(value(x) for x in X))
+    A += s
+    # print(s)
   print(A)
 
 
