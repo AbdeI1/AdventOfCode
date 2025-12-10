08ba7838305c36d702721588ae37e558fbb0b767
AbdeI1 <abdelrahmanmmokbel@gmail.com>
2025-12-09 23:48:21 -0600

    2025 day 10

--- 2025/10/code.py
+++ 2025/10/code.py
@@ -1,5 +1,7 @@
 import os
 os.chdir(os.path.dirname(__file__))
+from collections import deque
+from heapq import heappop, heappush
 
 
 def reader():
@@ -7,11 +9,33 @@ def reader():
 
 
 def part1():
-  pass
+  f = [(l[1:l.find(']')], list(map(lambda x: eval(f'{{{x[1:-1]}}}'), l[l.find('('):l.find('{')-1].split())), eval(f'[{l[l.find('{')+1:-1]}]')) for l in reader()]
+  A = 0
+  for E, B, _ in f:
+    E = tuple(1 if c == '#' else 0 for c in E)
+    S = (0,) * len(E)
+    Q = deque([(S, 0)])
+    V = set()
+    while len(Q) > 0:
+      s, d = Q.popleft()
+      if s in V:
+        continue
+      V.add(s)
+      if s == E:
+        A += d
+        break
+      for b in B:
+        ns = tuple(s[i] if i not in b else (1 - s[i]) for i in range(len(s)))
+        Q.append((ns, d + 1))
+  print(A)
 
 
 def part2():
-  pass
+  f = [(l[1:l.find(']')], list(map(lambda x: eval(f'{{{x[1:-1]}}}'), l[l.find('('):l.find('{')-1].split())), eval(f'({l[l.find('{')+1:-1]})')) for l in reader()]
+  A = 0
+  for _, B, E in f:
+    print([min(E[i] for i in b) for b in B])
+  print(A)
 
 
 part1()
--- wrapper.py
+++ wrapper.py
@@ -60,7 +60,7 @@ def submit(year, day):
     aocd_submit(out[1], part="b", year=year, day=day, session=token)
 
 
-y, d = 2025, 9
+y, d = 2025, 10
 
 fetch(y, d)
 submit(y, d)
